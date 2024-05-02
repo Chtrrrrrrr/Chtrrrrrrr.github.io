6a5308ecf226188df9d9a8861444482d721a612f
Chtrrrrrrr <81063418+CTRTiger@users.noreply.github.com>
2024-05-02 23:23:38 +0800

    add

---
+++ chtrrrrrrrwriting/articles/DisplayPDF.php
@@ -0,0 +1,424 @@
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <title>Chtrrrrrrr Writing</title>
+    <link rel="icon" href="\img\Icon.ico" sizes="16x16">
+</head>
+<style>
+            * {
+                box-sizing: border-box;
+            }
+
+            body {
+                margin: 0;
+            }
+
+            * {
+                box-sizing: border-box;
+            }
+
+            body {
+                margin-top: 0px;
+                margin-right: 0px;
+                margin-bottom: 0px;
+                margin-left: 0px;
+                font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", Helvetica, Roboto, Arial, "PingFang SC", "Hiragino Sans GB", "Microsoft Yahei", "Microsoft Jhenghei", sans-serif;
+            }
+
+            .panel {
+                color: rgb(51, 51, 51);
+                font-weight: 100;
+            }
+
+            .logo path {
+                pointer-events: none;
+                fill: none;
+                stroke-linecap: round;
+                stroke-width: 7;
+                stroke: rgb(255, 255, 255);
+            }
+
+            .description {
+                text-align: center;
+                font-size: 1em;
+                font-weight: 500;
+                line-height: 1.8rem;
+                margin-bottom: 1rem;
+            }
+
+            .wechat-group img {
+                max-width: 220px;
+                height: auto;
+                border-top-left-radius: 8px;
+                border-top-right-radius: 8px;
+                border-bottom-right-radius: 8px;
+                border-bottom-left-radius: 8px;
+                margin-top: 0px;
+                margin-right: auto;
+                margin-bottom: 0px;
+                margin-left: auto;
+                width: 100%;
+            }
+
+            .welcome-img img {
+                width: 100%;
+            }
+
+            .bg-gradient-primary {
+                border-top-left-radius: 8px;
+                border-top-right-radius: 8px;
+                border-bottom-right-radius: 8px;
+                border-bottom-left-radius: 8px;
+            }
+
+
+            .dropdown-lg-content {
+                width: 62px;
+            }
+
+            .dropdown-menu.dropdown-lg {
+                width: 37px;
+            }
+
+            .htmlpage-row {
+                display: table;
+                padding-top: 10px;
+                padding-right: 10px;
+                padding-bottom: 10px;
+                padding-left: 10px;
+                width: 100%;
+            }
+
+            .htmlpage-cell {
+                width: 8%;
+                display: table-cell;
+                height: 75px;
+            }
+
+            .fdb-block {
+                font-family: Roboto, sans-serif;
+                font-size: 16px;
+                line-height: 1.5;
+                text-rendering: optimizelegibility;
+                padding-top: 100px;
+                padding-right: 0px;
+                padding-bottom: 100px;
+                padding-left: 0px;
+                color: rgb(68, 68, 68);
+                background-size: cover;
+                background-position-x: center;
+                background-position-y: center;
+                overflow-x: hidden;
+                overflow-y: hidden;
+            }
+
+            .align-items-center {
+                align-items: center;
+            }
+
+            .fdb-block img {
+                border-top-left-radius: 4px;
+                border-top-right-radius: 4px;
+                border-bottom-right-radius: 4px;
+                border-bottom-left-radius: 4px;
+            }
+
+            .fdb-block .fdb-touch {
+                border-top-width: 5px;
+                border-top-style: solid;
+                border-top-color: rgb(82, 139, 255);
+            }
+
+            .fdb-block .fdb-box {
+                background-image: initial;
+                background-position-x: initial;
+                background-position-y: initial;
+                background-size: initial;
+                background-repeat: initial;
+                background-attachment: initial;
+                background-origin: initial;
+                background-clip: initial;
+                background-color: rgb(255, 255, 255);
+                color: rgb(68, 68, 68);
+                padding-top: 60px;
+                padding-right: 40px;
+                padding-bottom: 60px;
+                padding-left: 40px;
+                border-top-left-radius: 4px;
+                border-top-right-radius: 4px;
+                border-bottom-right-radius: 4px;
+                border-bottom-left-radius: 4px;
+                box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+                overflow-x: hidden;
+                overflow-y: hidden;
+            }
+
+            .fdb-block .text-h2,
+            .fdb-block h2 {
+                font-size: 1rem;
+                margin-bottom: 0.5em;
+                margin-top: 0.3em;
+                font-weight: 400;
+            }
+
+            .fdb-block .text-h3,
+            .fdb-block h3 {
+                font-size: 0.875rem;
+                margin-bottom: 0.5em;
+                margin-top: 0.3em;
+                font-weight: 400;
+            }
+
+            .mt-4 {
+                margin-top: 1.5rem !important;
+            }
+
+            a {
+                color: rgb(0, 123, 255);
+                text-decoration-line: none;
+                text-decoration-thickness: initial;
+                text-decoration-style: initial;
+                text-decoration-color: initial;
+                background-color: transparent;
+            }
+
+            a:hover {
+                color: rgb(0, 86, 179);
+                text-decoration-line: underline;
+                text-decoration-thickness: initial;
+                text-decoration-style: initial;
+                text-decoration-color: initial;
+            }
+
+            .c7883 {
+                background-image: url(img/bg_3.svg);
+            }
+
+
+            .c16864 {
+                color: black;
+                width: 84px;
+                height: 86.34881591796875px;
+            }
+
+            .c17074 {
+                padding: 10px;
+                color: #000000;
+                font-family: Arial, Helvetica, sans-serif;
+                font-size: 49px;
+            }
+
+            .c17251 {
+                padding: 10px;
+                color: #000000;
+                font-size: 20px;
+            }
+
+            .c18244 {
+                padding: 10px;
+                font-family: "Microsoft Jhenghei", sans-serif;
+                font-size: 38px;
+                text-align: center;
+            }
+
+            @media (max-width: 768px) {
+                .htmlpage-cell {
+                    width: 100%;
+                    display: block;
+                }
+            }
+            .textdiv {
+        width: 88%;
+        margin: auto;
+        top: 30px;
+        bottom: 50px;
+        background-color: white;
+        border: 3px solid white;
+        border-radius: 9px;
+        opacity: 0.87;
+        text-align: center;
+    }
+    .moddiv {
+        width: 250px;
+        height: 250px;
+        border: 3px solid rgb(208, 208, 208);
+        background-color: rgb(255, 255, 255);
+        border-radius: 9px;
+        opacity: 1.0;
+        display:inline-block;
+        overflow: hidden;
+        vertical-align: top;
+    }
+    html, body {
+			scroll-behavior:smooth;
+		}
+    .linedpaper {
+        background-color: #fff;
+background-image:
+linear-gradient(90deg, transparent 79px, #abced4 79px, #abced4 81px, transparent 81px),
+linear-gradient(#eee .1em, transparent .1em);
+background-size: 100% 1.2em;
+    }
+        </style>
+<body class="linedpaper">
+    <div>
+        <div id="default-editor-page__placeholder" class="panel">
+            <link rel="stylesheet" href="css/plugins.css" />
+            <link rel="stylesheet" href="css/style.css" />
+            <section class="wrapper bg-gradient-primary"><!-- /.container --></section>
+            <section class="wrapper bg-gradient-primary"></section>
+        </div>
+        <link rel="stylesheet" href="css/plugins.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <script src="js/plugins.js"></script>
+        <script src="js/theme.js"></script>
+        <link rel="stylesheet" href="css/plugins.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <script src="js/plugins.js"></script>
+        <script src="js/theme.js"></script>
+        <link rel="stylesheet" href="css/plugins.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <script src="js/plugins.js"></script>
+        <script src="js/theme.js"></script>
+        <div class="htmlpage-row">
+            <div class="htmlpage-cell">
+                <link rel="stylesheet" href="css/bootstrap.min.css" />
+                <link rel="stylesheet" href="css/plugins.css" />
+                <link rel="stylesheet" href="css/style.css" />
+                <script src="js/plugins.js"></script>
+                <script src="js/theme.js"></script>
+                <link rel="stylesheet" href="css/plugins.css" />
+                <link rel="stylesheet" href="css/style.css" />
+                <header class="wrapper bg-soft-primary pb-10">
+                    <nav class="navbar navbar-expand-lg fancy navbar-light navbar-bg-light">
+                        <div class="container">
+                            <div
+                                class="navbar-collapse-wrapper bg-white d-flex flex-row flex-nowrap w-100 justify-content-between align-items-center">
+                                <img src="img/Icon.png" class="c16864" />
+                                <div class="navbar-collapse offcanvas offcanvas-nav offcanvas-start">
+                                    <div class="offcanvas-header d-lg-none">
+                                        <h3 class="text-white fs-30 mb-0">HTMLPAGE.CN</h3><button type="button"
+                                            data-bs-dismiss="offcanvas" aria-label="Close"
+                                            class="btn-close btn-close-white">Send</button>
+                                    </div>
+                                    <div id="iyn9pb" class="c17074">Chtrrrrrrr Writing</div>
+                                    <div id="ickmrl" class="c17251">An Individual Article Website 🖊<br /></div>
+                                    <div class="offcanvas-body ms-lg-auto d-flex flex-column h-100">
+                                        <ul class="navbar-nav">
+                                            <li class="nav-item"><a href="..\index.html" class="nav-link">首页</a></li>
+                                            <li class="nav-item dropdown">
+                                                <ul class="dropdown-menu">
+                                                    <li class="nav-item"><a href="#" class="dropdown-item">用户体验</a></li>
+                                                    <li class="dropdown dropdown-submenu dropend"><a href="#"
+                                                            data-bs-toggle="dropdown"
+                                                            class="dropdown-item dropdown-toggle">视觉设计</a>
+                                                        <ul class="dropdown-menu">
+                                                            <li class="nav-item"><a href="#"
+                                                                    class="dropdown-item">界面设计</a></li>
+                                                            <li class="nav-item"><a href="#"
+                                                                    class="dropdown-item">平面设计</a></li>
+                                                        </ul>
+                                                    </li>
+                                                    <li class="nav-item"><a href="#" class="dropdown-item">架构设计</a></li>
+                                                </ul>
+                                            </li>
+                                            <li class="nav-item dropdown dropdown-mega">
+                                                <ul class="dropdown-menu mega-menu">
+                                                    <li class="mega-menu-content">
+                                                        <div class="row gx-0 gx-lg-3">
+                                                            <div class="col-lg-6">
+                                                                <h6 class="dropdown-header">体验设计</h6>
+                                                                <div class="row gx-0">
+                                                                    <div class="col-lg-6">
+                                                                        <ul class="list-unstyled">
+                                                                            <li><a href="#"
+                                                                                    class="dropdown-item">某电商品牌</a></li>
+                                                                            <li><a href="#"
+                                                                                    class="dropdown-item">某电商品牌</a></li>
+                                                                        </ul>
+                                                                    </div><!--/column -->
+                                                                </div><!--/.row -->
+                                                            </div><!--/column -->
+                                                            <div class="col-lg-3">
+                                                                <h6 class="dropdown-header">交互设计</h6>
+                                                                <ul class="list-unstyled">
+                                                                    <li><a href="#" class="dropdown-item">某游戏平台</a></li>
+                                                                    <li><a href="#" class="dropdown-item">某游戏平台</a></li>
+                                                                </ul>
+                                                            </div><!--/column -->
+                                                        </div><!--/.row -->
+                                                    </li><!--/.mega-menu-content-->
+                                                </ul><!--/.dropdown-menu -->
+                                            </li>
+                                            <li class="nav-item dropdown">
+                                                <div class="dropdown-menu dropdown-lg">
+                                                    <div class="dropdown-lg-content">
+                                                        <div>
+                                                            <h6 class="dropdown-header">企业介绍</h6>
+                                                            <ul class="list-unstyled">
+                                                                <li><a href="#" class="dropdown-item">企业文化</a></li>
+                                                                <li><a href="#" class="dropdown-item">福利待遇</a></li>
+                                                            </ul>
+                                                        </div><!-- /.column -->
+                                                        <div>
+                                                            <h6 class="dropdown-header">联系方式</h6>
+                                                            <ul class="list-unstyled">
+                                                                <li><a href="#" class="dropdown-item">联系电话</a></li>
+                                                                <li><a href="#" class="dropdown-item">电子邮件</a></li>
+                                                            </ul>
+                                                        </div><!-- /.column -->
+                                                    </div><!-- /auto-column -->
+                                                </div>
+                                            </li>
+                                        </ul><!-- /.navbar-nav -->
+                                        <div class="offcanvas-footer d-lg-none">
+                                            <div><a href="mailto:first.last@email.com"
+                                                    class="link-inverse">help@lapus.cn</a><br /> 00 (123) 456 78 90
+                                                <br />
+                                                <nav class="nav social social-white mt-4"><a href="#"><i
+                                                            class="uil uil-twitter"></i></a><a href="#"><i
+                                                            class="uil uil-facebook-f"></i></a><a href="#"><i
+                                                            class="uil uil-dribbble"></i></a><a href="#"><i
+                                                            class="uil uil-instagram"></i></a><a href="#"><i
+                                                            class="uil uil-youtube"></i></a></nav><!-- /.social -->
+                                            </div>
+                                        </div><!-- /.offcanvas-footer -->
+                                    </div><!-- /.offcanvas-body -->
+                                </div><!-- /.navbar-collapse -->
+                                <div class="navbar-other ms-lg-4">
+                                    <ul class="navbar-nav flex-row align-items-center ms-auto">
+                                        <li class="nav-item d-none d-md-block"><a href="https://chtrrrrrrr.mysxl.cn/"
+                                                data-bs-toggle="modal" data-bs-target="trueLink"
+                                                class="btn btn-sm btn-primary rounded-pill">联系我们</a></li>
+                                        <li class="nav-item d-lg-none"><button type="button"
+                                                class="hamburger offcanvas-nav-btn">Send</button></li>
+                                    </ul><!-- /.navbar-nav -->
+                                </div><!-- /.navbar-other -->
+                            </div><!-- /.navbar-collapse-wrapper -->
+                        </div><!-- /.container -->
+                    </nav><!-- /.navbar -->
+                </header>
+                <script src="js/plugins.js"></script>
+                <script src="js/theme.js"></script>
+                
+            </div>
+        </div>
+        <?php 
+            $Filename=$_GET["n"];
+    $FileTitle=$_GET["t"];
+    $Info=$_GET["i"];
+            echo "
+            <div class=\"textdiv\" id=\"Display\">
+            <h2>{$FileTitle}</h2><p>{$Info}|<a href=\"#down\">下载</a></p>
+            <object data=\"{$Filename}\" type=\"application/pdf\" width=100% height=200%>
+                <p>抱歉，您的浏览器不支持PDF预览，请自行下载PDF文件。</p>
+            </object><hr>
+            <h3 id=\"down\">下载此论文PDF：<a href=\"{$Filename}\" download=\"ChtWr:{$Info}:{$Filename}\">点击下载</a>（这个可是不要钱的呢，夺良心啊（bushi）</h3><hr>
+            </div>";
+            ?>
+        <div class="description">© 2024 Chtrrrrrrr. 版权所有</div>
+    </div>
+</body>
+
+</html>
